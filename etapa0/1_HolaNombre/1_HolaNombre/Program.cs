32424cb90dee64d9aa325f2b3f90195d8cd27fa8
azapaE <167466849+azapaE@users.noreply.github.com>
2024-05-28 16:55:13 -0300

    1_holanombre

---
+++ etapa0/1_HolaNombre/1_HolaNombre/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("comote te llamas"); 
+
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("hola" + nombre);
+
+            Console.ReadKey();
+
+        }
+    }
+}
