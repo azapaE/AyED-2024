8a58bec8c0099b9c7399b567210e8f546f07e0f7
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-29 16:11:12 -0300

    2_ajllahuanca_testvariables

---
+++ etapa0/2_ajllahuanca_testvariables/2_ajllahuanca_testvariables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_ajllahuanca_testvariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena= "152,37295892303";
+            int entero= 152;
+            bool booleano= false;
+            double doble= 152.37295892303;
+            float flotante= 152.37295892303f;
+            char caracter = '1';
+            Console.WriteLine(cadena);
+            Console.WriteLine(entero);
+            Console.WriteLine(booleano);
+            Console.WriteLine(doble);
+            Console.WriteLine(flotante);
+            Console.WriteLine(caracter);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
