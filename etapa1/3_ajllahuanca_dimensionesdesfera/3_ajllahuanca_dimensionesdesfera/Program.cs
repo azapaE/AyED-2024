bb42876f9531ae7e770f6999eb9b1fc57d903277
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-29 16:41:29 -0300

    dimensionesdes

---
+++ etapa1/3_ajllahuanca_dimensionesdesfera/3_ajllahuanca_dimensionesdesfera/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_ajllahuanca_dimensionesdesfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduce un numero para el radio de la esfera");
+            int radio = int.Parse(Console.ReadLine());
+            float pi = 3.14f;
+            Console.WriteLine("la supericie de la esfera es " + (4 * pi * radio * radio));
+            Console.WriteLine("el volumen de la esfera es " + ((3 * pi * radio * radio * radio) / 4));
+            Console.ReadKey();
+        }
+    }
+}
