f71a613dc11bcb057824fb2b853f2731bc864a3c
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-29 16:12:25 -0300

    0_CalculadoraBasica

---
+++ etapa1/0_CalculadoraBasica/0_CalculadoraBasica/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_CalculadoraBasica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero");
+            int numero2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(numero + " + " + numero2 + " = " + (numero + numero2));
+            Console.WriteLine(numero + " - " + numero2 + " = " + (numero - numero2));
+            Console.WriteLine(numero + " * " + numero2 + " = " + (numero * numero2));
+            Console.WriteLine(numero + " / " + numero2 + " = " + (numero / numero2));
+            Console.WriteLine("el resto de " + numero + "  y  " + numero2 + " es " + (numero % numero2));
+            Console.ReadKey();
+        }
+    }
+}
