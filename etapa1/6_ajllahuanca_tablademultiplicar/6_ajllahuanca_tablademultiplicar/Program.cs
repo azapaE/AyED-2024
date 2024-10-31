96ab09bc4eb42627424f44692a0cd1d98bf0f957
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-31 15:12:45 -0300

    6_tablademultiplicar

---
+++ etapa1/6_ajllahuanca_tablademultiplicar/6_ajllahuanca_tablademultiplicar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_ajllahuanca_tablademultiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa un numero");
+            int num = int.Parse(Console.ReadLine());
+            int num2 = 1;
+            while (num2 < 11)
+            {
+                Console.WriteLine(num + " * 1= " + (num * num2));
+                num2++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
