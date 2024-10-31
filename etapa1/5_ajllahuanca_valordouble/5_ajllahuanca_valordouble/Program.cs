9a47cdef35547d5c4324b1f806acd32004b62670
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-31 15:09:33 -0300

    5_valordouble

---
+++ etapa1/5_ajllahuanca_valordouble/5_ajllahuanca_valordouble/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ajllahuanca_valordouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dame una altura");
+            int altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("dame un ancho");
+            int ancho = int.Parse(Console.ReadLine());
+            Console.WriteLine("el perimetro del rectangulo es " + (ancho * 2 + altura * 2));
+            Console.WriteLine("el area del rectangulo es " + (ancho * altura));
+            Console.WriteLine("la diagonal del triangulo es " + Math.Sqrt((ancho * ancho + altura * altura)));
+            Console.ReadKey();
+        }
+    }
+}
