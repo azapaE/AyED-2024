32737e2d9f9abee26bcae0feb2d13cd96563247c
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-31 15:08:33 -0300

    Add files via upload

---
+++ etapa1/4_centigradoskelvinafarenheit/4_centigradoskelvinafarenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_centigradoskelvinafarenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa una cantidad de grados pare convertirlos en Kelvin y Farenheit");
+            int grados = int.Parse(Console.ReadLine());
+            Console.WriteLine("Kelvin:" + (grados + 273));
+            Console.WriteLine("Farenheit:" + ((grados * 18) / 10) + 32);
+            Console.ReadKey();
+        }
+    }
+    }
+}
