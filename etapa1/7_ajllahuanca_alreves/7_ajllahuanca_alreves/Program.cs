470cb485ac2f7a7a9f85be12762e7452550b9802
azapaE <167466849+azapaE@users.noreply.github.com>
2024-10-31 16:17:58 -0300

    Add files via upload

---
+++ etapa1/7_ajllahuanca_alreves/7_ajllahuanca_alreves/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ajllahuanca_alreves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa una letra");
+            char letra = char.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa otra letra");
+            char letra1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("ingresa otra letra");
+            char letra2 = char.Parse(Console.ReadLine());
+            Console.WriteLine(letra2);
+            Console.WriteLine(letra1);
+            Console.WriteLine(letra);
+            Console.ReadKey();
+
+        }
+    }
+}
