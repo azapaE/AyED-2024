b5c43a6decbe222b312bca954222fc9df5882a60
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-05 16:13:43 -0300

    8_ajllahuanca_EsMayor

---
+++ etapa1/8_ajllahuanca_Esmayor/8_ajllahuanca_Esmayor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_ajllahuanca_Esmayor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa su edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad < 18)
+            {
+                Console.WriteLine("sos menor de edad");
+            }
+            else if (edad > 18)
+            {
+                Console.WriteLine("sos mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("sos mayor");
+            }
+            Console.ReadKey();
+        }
+    }
+}
