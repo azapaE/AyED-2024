830f67db2d92948d29c46ad6a2df0bafd5d69212
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-05 16:20:35 -0300

    9_ajllahuanca_ingresepassword

---
+++ etapa1/9_ajllahuanca_ingresepassword/9_ajllahuanca_ingresepassword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_ajllahuanca_ingresepassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese la contraseña");
+            String contraseña = Console.ReadLine();
+            String contra = "ariel";
+            if (contraseña == contra)
+            {
+                Console.WriteLine("contraseña correcta");
+            }
+            else
+            {
+                Console.WriteLine("contraseña incorrecta");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
