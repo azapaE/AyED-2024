19baa8014b662e8cb0d1a1b604c39251606726aa
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-05 16:33:26 -0300

    11_ajllahunca_paroimpar

---
+++ etapa1/11_paroimpar/11_paroimpar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_paroimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Escribir un programa que pida al usuario un número entero
+               y muestre por pantalla si es par o impar*/
+            Console.WriteLine("escriba un numero entero");
+            int num = int.Parse(Console.ReadLine());
+
+            if ((num % 2)== 0) {
+                Console.WriteLine("el numero es par");
+                 }
+                else {
+                Console.WriteLine("el numero es impar");
+                 }
+                Console.ReadKey();
+
+             
+        }
+    }
+}
