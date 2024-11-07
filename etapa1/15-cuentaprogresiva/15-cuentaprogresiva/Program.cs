215d568c57bbb43d1237e5d2a0764b0ae1d05ffc
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-07 14:36:15 -0300

    15_ajllahuanca_cuentaprogresiva

---
+++ etapa1/15-cuentaprogresiva/15-cuentaprogresiva/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_cuentaprogresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribir un programa que pida al usuario un número entero positivo
+              y muestre por pantalla la cuenta atrás desde ese número hasta cero.*/
+            Console.WriteLine("escribe un numero entero positivo");
+            int numero = int.Parse(Console.ReadLine());
+            int contador = -1;
+            int cuentareg = numero;
+            while (contador < numero) {
+                Console.WriteLine(cuentareg-1+1);
+                cuentareg -= 1;
+                contador += 1;
+                 }
+            Console.ReadKey();
+
+
+        }
+    }
+}
