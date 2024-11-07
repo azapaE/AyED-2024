bad6e1185318e11ed619f56256883094d289da25
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-07 14:31:22 -0300

    14_ajllahuanca_cumplidos

---
+++ etapa1/14_cumplidos/14_cumplidos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_cumplidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Escribir un programa que pregunte al usuario su edad
+             *  y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).*/
+            Console.WriteLine("¿cual es tu edad ?");
+            int edad = int.Parse(Console.ReadLine());
+            int e = edad;
+            int contador = 0;
+
+            while (contador < edad) {
+                Console.WriteLine(e-edad + 1);
+                e += 1;
+                contador += 1;
+               
+                 }
+
+
+
+
+
+                Console.ReadKey();
+        }
+    }
+}
