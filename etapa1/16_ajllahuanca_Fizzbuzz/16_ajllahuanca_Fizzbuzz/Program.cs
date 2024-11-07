687f4946d8b6cd55e9c5d3bc41dc3e5f966202e1
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-07 15:01:09 -0300

    16_ajllahuanca_fizzbuzz

---
+++ etapa1/16_ajllahuanca_Fizzbuzz/16_ajllahuanca_Fizzbuzz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_ajllahuanca_Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            while (num < 101)
+            {
+                if (!(num % 3 == 0 & num % 5 == 0))
+                {
+                    if (!(num % 5 == 0))
+                    {
+                        if (!(num % 3 == 0))
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(num);
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("fizz");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("buzz");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("fizzbuzz");
+                }
+
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
