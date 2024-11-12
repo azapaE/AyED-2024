36f3d0d6a5355ed3b77d6330e903a63e1c2370d7
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-12 15:14:30 -0300

    19_ajllahuanca_hastaN

---
+++ etapa1/19_ajllahuanca_HastaN/19_ajllahuanca_HastaN/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_ajllahuanca_HastaN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingresa un número");
+            int n = int.Parse(Console.ReadLine());
+            int n2 = 1;
+            while (n2 < n + 1)
+            {
+                if (n2 % 2 == 0)
+                {
+                    Console.WriteLine(n2);
+                }
+                n2++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
