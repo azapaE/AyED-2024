ab24e0789551bbc7f064fc805bc3df2fcfc10f6d
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-12 14:33:06 -0300

    18_ajllahuanca_Hastaquesi

---
+++ etapa1/18_ajllahuanca_Hastaquesi/18_ajllahuanca_Hastaquesi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_ajllahuanca_Hastaquesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool bucle = true;
+            String si = "SI";
+            while (bucle)
+            {
+                Console.WriteLine("Deseas terminar el programa");
+                String respuestas = Console.ReadLine();
+
+                if (respuestas == si)
+                {
+                    bucle = false;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
