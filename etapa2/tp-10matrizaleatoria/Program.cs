0a91846ee5a5107c0b0fb408d9bcae0d293d6d1f
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 00:41:13 +0100

    tp10_ajllahuanca_matrizaleatoria

---
+++ etapa2/tp-10matrizaleatoria/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_10matrizaleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/**/
+            Console.WriteLine("matriz con rango random");
+            Console.WriteLine("ingrese N y M ");
+            Random aleatorio = new Random();
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[N, M];
+            for (int c = 0; c < M; c++)
+            {
+
+                for (int f = 0; f < N; f++)
+                {
+                    matriz[f, c] = aleatorio.Next(0, 100);
+                    Console.Write(matriz[f, c] + "  \t ");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
