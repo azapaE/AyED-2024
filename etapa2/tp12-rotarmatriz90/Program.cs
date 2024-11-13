52f9b4e86aee5dcd77317479428115306eb4d26e
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 00:42:31 +0100

    tp12_ajllahuanca_rotarmatriz90

---
+++ etapa2/tp12-rotarmatriz90/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp12_rotarmatriz90
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/**/
+            Console.WriteLine("ingrese el el valor de N y M");
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            Random aleatorio = new Random();
+            int[,] matriz = new int[N, M];
+            for (int c = 0; c < N; c++)
+            {
+
+                for (int f = 0; f < M; f++)
+                {
+                    matriz[c, f] = aleatorio.Next(0, 100);
+                    Console.Write(matriz[c, f] + " \t ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            int[,] matriz2 = new int[N, M];
+            for (int f = 0; f < M; f++)
+            {
+
+                for (int c = 0; c < N; c++)
+                {
+                    matriz2[c, f] = matriz[c, f];
+                    Console.Write(matriz2[c, f] + " \t ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
