889257d2d4db23f795a071d37a3cdc083cba69b8
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 00:43:13 +0100

    tp13_ajllahuanca_obtenerdiagonalprincipal

---
+++ etapa2/tp13-obtenerdiagonalprincipal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp13_obtenerdiagonalprincipal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/**/
+            Console.WriteLine("ingrese el el valor de N ");
+            int N = int.Parse(Console.ReadLine());
+            Random aleatorio = new Random();
+            int[,] matriz = new int[N, N];
+            for (int c = 0; c < N; c++)
+            {
+
+                for (int f = 0; f < N; f++)
+                {
+                    matriz[c, f] = aleatorio.Next(0, 100);
+                    Console.Write(matriz[c, f] + " \t ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            int[] vector = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                vector[i] = matriz[i, i];
+                Console.Write(vector[i] + " \t ");
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
