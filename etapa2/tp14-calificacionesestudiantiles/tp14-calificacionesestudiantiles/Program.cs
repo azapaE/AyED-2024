cab897c79b54808dbcdf13614934c536d9d305c4
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 00:43:55 +0100

    tp14_ajllahuanca_calificacionesestudiantiles

---
+++ etapa2/tp14-calificacionesestudiantiles/tp14-calificacionesestudiantiles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp14_calificacionesestudiantiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/**/
+            Console.WriteLine("ingrese la cantidad de Estudiantes");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("colocar Nombre, edad y calificacion en este orden");
+            Console.WriteLine();
+            string[,] matriz = new string[3, N];
+            for (int c = 0; c < N; c++)
+            {
+
+                for (int f = 0; f < 3; f++)
+                {
+                    matriz[f, c] = Console.ReadLine();
+
+                    Console.Clear();
+                }
+                Console.WriteLine();
+            }
+            for (int c = 0; c < N; c++)
+            {
+
+                for (int f = 0; f < 3; f++)
+                {
+                    Console.Write(matriz[f, c] + "\t");
+
+
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
