33094201cca65802b74bca4869c33f45f6509ba9
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 00:40:25 +0100

    tp9_ajllahuanca_miprimermatriz

---
+++ etapa2/tp9_miprimermatriz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp9_miprimermatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*Crear una matriz de n×m con todos sus elementos inicializados
+         a cero.  Imprimirla por pantalla
+         n y m son datos que ingresa el usuario*/
+
+            Console.WriteLine("ingrese el el valor de N y M");
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[N, M];
+            for (int c = 0; c < M; c++)
+            {
+
+                for (int f = 0; f < N; f++)
+                {
+                    matriz[f, c] = 0;
+                    Console.Write(matriz[f, c]+"\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
