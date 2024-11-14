202eb5f715fde9e65144463a352d52d9910b3125
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 11:27:37 -0300

    2 - ajllahuanca-AreaVolumen

---
+++ etapa3/2 - AreaVolumen/2 - AreaVolumen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___AreaVolumen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese el radio del circurlo ");
+            int radio = int.Parse(Console.ReadLine());
+            Console.Write("ingrese el alto del cilindro ");
+            int altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("el area del circulo es: " + area(radio));
+            Console.WriteLine("el volumen del cilindro es: " + volumen(radio, altura));
+            Console.ReadKey();
+        }
+        static double area(double r)
+        {
+            r = Math.PI * r;
+            return r;
+        }
+        static double volumen(double volumen, int h)
+        {
+            volumen = Math.PI * (volumen*volumen)*h;
+            return volumen;
+        }
+    }
+}
