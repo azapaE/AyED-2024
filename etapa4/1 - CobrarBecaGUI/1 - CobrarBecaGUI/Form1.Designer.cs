c2c6fb18b9ef853facc6599c95dadb15217f141d
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 11:33:45 -0300

    1 - ajllahuanca-CobrarBecaGUI

---
+++ etapa4/1 - CobrarBecaGUI/1 - CobrarBecaGUI/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace _1___CobrarBecaGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Edad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ingresos = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese su Edad";
+            // 
+            // Edad
+            // 
+            this.Edad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Edad.Location = new System.Drawing.Point(60, 93);
+            this.Edad.Name = "Edad";
+            this.Edad.Size = new System.Drawing.Size(161, 26);
+            this.Edad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Seleccione sus Ingresos Mensuales";
+            // 
+            // Ingresos
+            // 
+            this.Ingresos.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ingresos.FormattingEnabled = true;
+            this.Ingresos.Items.AddRange(new object[] {
+            "Entre 0 - 50,000",
+            "50,001 - 100,000",
+            "100,001 - 200,000",
+            "Más de 200,000"});
+            this.Ingresos.Location = new System.Drawing.Point(60, 176);
+            this.Ingresos.Name = "Ingresos";
+            this.Ingresos.Size = new System.Drawing.Size(161, 24);
+            this.Ingresos.TabIndex = 3;
+            this.Ingresos.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 324);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Ingresos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Edad);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Edad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox Ingresos;
+    }
+}
+
---
+++ etapa4/1 - CobrarBecaGUI/1 - CobrarBecaGUI/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1___CobrarBecaGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year;
+            year = int.Parse(Edad.Text);
+            if (year >= 19)
+            {
+                if(Ingresos.Text == "Entre 0 - 50,000")
+                {
+                    MessageBox.Show("No cumples con los requisitos para cobrar la beca");
+                }
+                else if((Ingresos.Text == "50,001 - 100,000") || (Ingresos.Text == "100,001 - 200,000") ||(Ingresos.Text == "Más de 200,000"))
+                {
+                    MessageBox.Show("¡Felicidades! Puedes cobrar la beca");
+                }
+                else
+                {
+                    MessageBox.Show("seleccione unas de las opciones");
+                }
+            }
+            else
+            {
+                MessageBox.Show("el usuario no cumple con la edad requerida");
+            }
+
+        }
+        
+    }
+}
