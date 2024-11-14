ee7c80add460f453871b2c877c221cd36a0eaa25
azapaE <167466849+azapaE@users.noreply.github.com>
2024-11-14 11:25:10 -0300

    0-ajllahaunca-miprimerfuncion

---
+++ etapa3/0 - MiPrimerFuncion/0 - MiPrimerFuncion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0___MiPrimerFuncion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese su nombre");
+            String name = Console.ReadLine();
+            Console.WriteLine(saludo(name));
+            Console.ReadKey();
+        }
+        static String saludo(String nombre)
+        {
+            Console.WriteLine("Hola" + nombre);
+            return nombre;
+        }
+    }
+}
